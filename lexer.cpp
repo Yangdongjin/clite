d5e2988bd0ea6b55942e158b0694b588a6fb28a6
david <david@weinman.com>
2013-07-20 21:24:04 -0700

    began writing lexer struct
    added header file.
    
    needing to come up with a style for this and documentation

--- lexer.cpp
+++ lexer.cpp
@@ -3,17 +3,38 @@
 // 7/17/13 11:30p
 
 
-static bool isEof = false;
-static char ch = ' ';
-static char[] line = "";
-int lineno = 0;
-int col = 1;
-char[] letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char[] digits = "0123456789";
-char eolnCh = '\n';
-char eofCh = '\004';
+struct lexer {
+
+	lexer(string filename);
+	Token next();
+	void error(String);
+
+	private:
+		static bool isEof = false;
+		static char ch = ' ';
+		static char[] line = "";
+		int lineno = 0;
+		int col = 1;
+		char[] letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		char[] digits = "0123456789";
+		char eolnCh = '\n';
+		char eofCh = '\004';
+		char nextChar();
+		bool isLetter(char);
+		bool isDigit(char);
+		void check(char);
+		Token checkOpt(char, Token, Token);
+		String concat(String);
+
+	
+
+};
+
+lexer::lexer(string filename) {
 
 
+
+}
 
 /* PSEUDO
 
---
+++ lexer.hpp
@@ -0,0 +1,30 @@
+//
+
+struct lexer {
+
+        lexer(string filename);
+        Token next();
+        void error(String);
+
+        private:
+                static bool isEof = false;
+                static char ch = ' ';
+                static char[] line = "";
+                int lineno = 0;
+                int col = 1;
+                char[] letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                char[] digits = "0123456789";
+                char eolnCh = '\n';
+                char eofCh = '\004';
+                char nextChar();
+                bool isLetter(char);
+                bool isDigit(char);
+                void check(char);
+                Token checkOpt(char, Token, Token);
+                String concat(String);
+
+
+
+};
+
+
