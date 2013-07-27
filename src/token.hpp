e9044263f95a232ac7655213edae97e856b3b295
David Weinman <david@weinman.com>
2013-07-27 14:53:22 -0700

    cleaned up indentation in token header

--- src/token.hpp
+++ src/token.hpp
@@ -8,45 +8,47 @@
 struct Token {
 
 	Token eofTok = Token(TokenType.Eof, "<<EOF>>");
-    Token boolTok = Token(TokenType.Bool, "bool");
-    Token charTok = Token(TokenType.Char, "char");
-    Token elseTok = Token(TokenType.Else, "else");
-    Token falseTok = Token(TokenType.False, "false");
-    Token floatTok = Token(TokenType.Float, "float");
-    Token ifTok = Token(TokenType.If, "if");
-    Token intTok = Token(TokenType.Int, "int");
-    Token mainTok = Token(TokenType.Main, "main");
+	Token boolTok = Token(TokenType.Bool, "bool");
+	Token charTok = Token(TokenType.Char, "char");
+	Token elseTok = Token(TokenType.Else, "else");
+	Token falseTok = Token(TokenType.False, "false");
+	Token floatTok = Token(TokenType.Float, "float");
+	Token ifTok = Token(TokenType.If, "if");
+	Token intTok = Token(TokenType.Int, "int");
+	Token mainTok = Token(TokenType.Main, "main");
 	Token trueTok = Token(TokenType.True, "true");
-    Token whileTok = Token(TokenType.While, "while");
-    Token leftBraceTok = Token(TokenType.LeftBrace, "{");
-    Token rightBraceTok = Token(TokenType.RightBrace, "}");
-    Token leftBracketTok = Token(TokenType.LeftBracket, "[");
-    Token rightBracketTok = Token(TokenType.RightBracket, "]");
-    Token leftParenTok = Token(TokenType.LeftParen, "(");
-    Token rightParenTok = Token(TokenType.RightParen, ")");
-    Token semicolonTok = Token(TokenType.Semicolon, ";");
-    Token commaTok = Token(TokenType.Comma, ",");
-    Token assignTok = Token(TokenType.Assign, "=");
-    Token eqeqTok = Token(TokenType.Equals, "==");
-    Token ltTok = Token(TokenType.Less, "<");
-    Token lteqTok = Token(TokenType.LessEqual, "<=");
-    Token gtTok = Token(TokenType.Greater, ">");
-    Token gteqTok = Token(TokenType.GreaterEqual, ">=");
-    Token notTok = Token(TokenType.Not, "!");
-    Token noteqTok = Token(TokenType.NotEqual, "!=");
-    Token plusTok = Token(TokenType.Plus, "+");
-    Token minusTok = Token(TokenType.Minus, "-");
-    Token multiplyTok = Token(TokenType.Multiply, "*");
-    Token divideTok = Token(TokenType.Divide, "/");
-    Token andTok = Token(TokenType.And, "&&");
-    Token orTok = Token(TokenType.Or, "||");
-    Token returnTok = Token(TokenType.Return, "return");
-    Token voidTok = Token(TokenType.Void, "void");
+	Token whileTok = Token(TokenType.While, "while");
+	Token leftBraceTok = Token(TokenType.LeftBrace, "{");
+	Token rightBraceTok = Token(TokenType.RightBrace, "}");
+	Token leftBracketTok = Token(TokenType.LeftBracket, "[");
+	Token rightBracketTok = Token(TokenType.RightBracket, "]");
+	Token leftParenTok = Token(TokenType.LeftParen, "(");
+	Token rightParenTok = Token(TokenType.RightParen, ")");
+	Token semicolonTok = Token(TokenType.Semicolon, ";");
+	Token commaTok = Token(TokenType.Comma, ",");
+	Token assignTok = Token(TokenType.Assign, "=");
+	Token eqeqTok = Token(TokenType.Equals, "==");
+	Token ltTok = Token(TokenType.Less, "<");
+	Token lteqTok = Token(TokenType.LessEqual, "<=");
+	Token gtTok = Token(TokenType.Greater, ">");
+	Token gteqTok = Token(TokenType.GreaterEqual, ">=");
+	Token notTok = Token(TokenType.Not, "!");
+	Token noteqTok = Token(TokenType.NotEqual, "!=");
+	Token plusTok = Token(TokenType.Plus, "+");
+	Token minusTok = Token(TokenType.Minus, "-");
+	Token multiplyTok = Token(TokenType.Multiply, "*");
+	Token divideTok = Token(TokenType.Divide, "/");
+	Token andTok = Token(TokenType.And, "&&");
+	Token orTok = Token(TokenType.Or, "||");
+	Token returnTok = Token(TokenType.Return, "return");
+	Token voidTok = Token(TokenType.Void, "void");
 
 	private:
 		int KEYWORDS = tokenType.Eof.ordinal();
 		char * reserved; //= new string of size KEYWORDS
 		Token[] token = Token[KEYWORDS];
+		TokenType type;
+		char * value;
 
 };
 
@@ -81,41 +83,41 @@ public class Token {
     private static final String[] reserved = new String[KEYWORDS];
     private static Token[] token = Token[KEYWORDS];
 
-    Token eofTok = Token(TokenType.Eof, "<<EOF>>");
-    Token boolTok = Token(TokenType.Bool, "bool");
-    Token charTok = Token(TokenType.Char, "char");
-    Token elseTok = Token(TokenType.Else, "else");
-    Token falseTok = Token(TokenType.False, "false");
-    Token floatTok = Token(TokenType.Float, "float");
-    Token ifTok = Token(TokenType.If, "if");
-    Token intTok = Token(TokenType.Int, "int");
-    Token mainTok = Token(TokenType.Main, "main");
-    Token trueTok = Token(TokenType.True, "true");
-    Token whileTok = Token(TokenType.While, "while");
-    Token leftBraceTok = Token(TokenType.LeftBrace, "{");
-    Token rightBraceTok = Token(TokenType.RightBrace, "}");
-    Token leftBracketTok = Token(TokenType.LeftBracket, "[");
-    Token rightBracketTok = Token(TokenType.RightBracket, "]");
-    Token leftParenTok = Token(TokenType.LeftParen, "(");
-    Token rightParenTok = Token(TokenType.RightParen, ")");
-    Token semicolonTok = Token(TokenType.Semicolon, ";");
-    Token commaTok = Token(TokenType.Comma, ",");
-    Token assignTok = Token(TokenType.Assign, "=");
-    Token eqeqTok = Token(TokenType.Equals, "==");
-    Token ltTok = Token(TokenType.Less, "<");
-    Token lteqTok = Token(TokenType.LessEqual, "<=");
-    Token gtTok = Token(TokenType.Greater, ">");
-    Token gteqTok = Token(TokenType.GreaterEqual, ">=");
-    Token notTok = Token(TokenType.Not, "!");
-    Token noteqTok = Token(TokenType.NotEqual, "!=");
-    Token plusTok = Token(TokenType.Plus, "+");
-    Token minusTok = Token(TokenType.Minus, "-");
-    Token multiplyTok = Token(TokenType.Multiply, "*");
-    Token divideTok = Token(TokenType.Divide, "/");
-    Token andTok = Token(TokenType.And, "&&");
-    Token orTok = Token(TokenType.Or, "||");
-    Token returnTok = Token(TokenType.Return, "return");
-    Token voidTok = Token(TokenType.Void, "void");
+Token eofTok = Token(TokenType.Eof, "<<EOF>>");
+Token boolTok = Token(TokenType.Bool, "bool");
+Token charTok = Token(TokenType.Char, "char");
+Token elseTok = Token(TokenType.Else, "else");
+Token falseTok = Token(TokenType.False, "false");
+Token floatTok = Token(TokenType.Float, "float");
+Token ifTok = Token(TokenType.If, "if");
+Token intTok = Token(TokenType.Int, "int");
+Token mainTok = Token(TokenType.Main, "main");
+Token trueTok = Token(TokenType.True, "true");
+Token whileTok = Token(TokenType.While, "while");
+Token leftBraceTok = Token(TokenType.LeftBrace, "{");
+Token rightBraceTok = Token(TokenType.RightBrace, "}");
+Token leftBracketTok = Token(TokenType.LeftBracket, "[");
+Token rightBracketTok = Token(TokenType.RightBracket, "]");
+Token leftParenTok = Token(TokenType.LeftParen, "(");
+Token rightParenTok = Token(TokenType.RightParen, ")");
+Token semicolonTok = Token(TokenType.Semicolon, ";");
+Token commaTok = Token(TokenType.Comma, ",");
+Token assignTok = Token(TokenType.Assign, "=");
+Token eqeqTok = Token(TokenType.Equals, "==");
+Token ltTok = Token(TokenType.Less, "<");
+Token lteqTok = Token(TokenType.LessEqual, "<=");
+Token gtTok = Token(TokenType.Greater, ">");
+Token gteqTok = Token(TokenType.GreaterEqual, ">=");
+Token notTok = Token(TokenType.Not, "!");
+Token noteqTok = Token(TokenType.NotEqual, "!=");
+Token plusTok = Token(TokenType.Plus, "+");
+Token minusTok = Token(TokenType.Minus, "-");
+Token multiplyTok = Token(TokenType.Multiply, "*");
+Token divideTok = Token(TokenType.Divide, "/");
+Token andTok = Token(TokenType.And, "&&");
+Token orTok = Token(TokenType.Or, "||");
+Token returnTok = Token(TokenType.Return, "return");
+Token voidTok = Token(TokenType.Void, "void");
 
     private TokenType type;
     private String value = "";
@@ -126,7 +128,7 @@ public class Token {
         if (t.compareTo(TokenType.Eof) < 0) {
             int ti = t.ordinal();
             reserved[ti] = v;
-            token[ti] = this;
+    Token[ti] = this;
         }
     }
 
